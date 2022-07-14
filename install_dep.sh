b66ed8dace9bc4fe307ac847dc5e068c4ead4068
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-15 01:01:18 +0200

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -4,8 +4,10 @@
    
 apt update
 apt -y install build-essential cmake git ruby-dev python3-pip || exit 1
+apt -y install python3-absl libc-ares-dev cmake curl libjsoncpp-dev protobuf-compiler libre2-dev tinyxml2 zlib1g
 
 gem install fpm
 pip3 install --upgrade cloudsmith-cli
 pip3 install future
+pip3 install grpcio
 
