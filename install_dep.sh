33008dc094b93296e341490b43c82aadc654628b
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-15 01:22:02 +0200

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -4,7 +4,7 @@
    
 apt update
 apt -y install build-essential cmake git ruby-dev python3-pip || exit 1
-apt -y install cmake curl libjsoncpp-dev libtinyxml2-6 zlib1g libcurl-dev
+apt -y install cmake curl libjsoncpp-dev libtinyxml2-dev zlib1g libcurl4-gnutls-dev
 
 gem install fpm
 pip3 install --upgrade cloudsmith-cli
