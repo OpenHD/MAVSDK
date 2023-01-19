db6d0aa5d4ffb7272797ffa4035bfb9ff77c4ca6
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-19 02:24:09 +0100

    clean way ?

--- tools/generate_mavlink_headers.sh
+++ tools/generate_mavlink_headers.sh
@@ -3,36 +3,39 @@
 set -e
 
 # Match hash from configure step
-REPO="https://github.com/OpenHD/mavlink.git"
-TAG="2.2.x-evo"
+REPO=$(grep GIT_REPOSITORY third_party/mavlink/CMakeLists.txt | awk '{print $2}')
+TAG=$(grep GIT_TAG third_party/mavlink/CMakeLists.txt | awk '{print $2}')
 MAVLINK_DIALECT=common
 
 MAVLINK_PATH=mavlink
 MAVLINK_HEADERS_PATH=mavlink-headers
 
-mkdir $MAVLINK_PATH
-echo $(pwd)
-cd $MAVLINK_PATH 
-git clone --recurse-submodules -b $TAG $REPO 
-ls
-cd mavlink 
-mv * ../
-ls
-echo "debug"
-cd ..
-ls
+if [ ! -d $MAVLINK_PATH ]
+then
+    git clone --recursive $REPO $MAVLINK_PATH
+else
+    (cd $MAVLINK_PATH && git fetch)
+fi
+
+(cd $MAVLINK_PATH && git checkout $TAG && git submodule update --init --recursive)
 
 OUTPUT_PATH="$MAVLINK_HEADERS_PATH/include/mavlink/v2.0/"
 mkdir -p "$OUTPUT_PATH"
 OUTPUT_PATH=$(realpath $OUTPUT_PATH)
 
-cd $MAVLINK_PATH && \
+echo "Installing dependencies"
+(cd $MAVLINK_PATH && \
+    python3 \
+        -m pip install -r pymavlink/requirements.txt)
+
+echo "Generating headers"
+(cd $MAVLINK_PATH && \
     python3 \
         -m pymavlink.tools.mavgen \
         --lang=C \
         --wire-protocol=2.0 \
         --output=$OUTPUT_PATH \
-        message_definitions/v1.0/$MAVLINK_DIALECT.xml
+        message_definitions/v1.0/$MAVLINK_DIALECT.xml)
 
 echo ""
 echo "Now run cmake with -DMAVLINK_HEADERS=\"$MAVLINK_HEADERS_PATH/include\""
