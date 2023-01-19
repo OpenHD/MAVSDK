5062766165223fe83ce8f413964ee815cd32aab6
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-19 02:08:34 +0100

    cleanup

--- tools/generate_mavlink_headers.sh
+++ tools/generate_mavlink_headers.sh
@@ -19,15 +19,13 @@ OUTPUT_PATH="$MAVLINK_HEADERS_PATH/include/mavlink/v2.0/"
 mkdir -p "$OUTPUT_PATH"
 OUTPUT_PATH=$(realpath $OUTPUT_PATH)
 
-tree
-
-(cd $MAVLINK_PATH && \
+cd $MAVLINK_PATH && \
     python3 \
         -m pymavlink.tools.mavgen \
         --lang=C \
         --wire-protocol=2.0 \
         --output=$OUTPUT_PATH \
-        message_definitions/v1.0/$MAVLINK_DIALECT.xml)
+        message_definitions/v1.0/$MAVLINK_DIALECT.xml
 
 echo ""
 echo "Now run cmake with -DMAVLINK_HEADERS=\"$MAVLINK_HEADERS_PATH/include\""
