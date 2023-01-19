ee04375a10aea96877e43c4307413fd645a7865e
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-19 01:40:08 +0100

    Update generate_mavlink_headers.sh

--- tools/generate_mavlink_headers.sh
+++ tools/generate_mavlink_headers.sh
@@ -11,7 +11,8 @@ MAVLINK_PATH=mavlink
 MAVLINK_HEADERS_PATH=mavlink-headers
 
 mkdir $MAVLINK_PATH
-cd $MAVLINK_PATH && git clone $REPO && git checkout $TAG && git submodule update --init --recursive
+cd $MAVLINK_PATH 
+git clone -b $TAG $REPO && git submodule update --init --recursive
 
 OUTPUT_PATH="$MAVLINK_HEADERS_PATH/include/mavlink/v2.0/"
 mkdir -p "$OUTPUT_PATH"
