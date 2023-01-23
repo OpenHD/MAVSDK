1257a6d9ebb55aac0320b912153d2a56adfd3a6e
consti10 <geierconstantinabc@gmail.com>
2023-01-23 03:04:29 +0100

    fix stupid folder structure

--- build_with_already_generated_mavlink.sh
+++ build_with_already_generated_mavlink.sh
@@ -1,2 +1,2 @@
-cmake -Bbuild/default -DMAVLINK_DIALECT=openhd -DMAVLINK_HEADERS=c_library_v2_openhd -H.
+cmake -Bbuild/default -DMAVLINK_DIALECT=openhd -DMAVLINK_HEADERS=mavlink-headers -H.
 cmake --build build/default -j4
